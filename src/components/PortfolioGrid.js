4bb910f5494848bf8a0f8046b5626ce42568168c
Darrin Wells <designrwells@gmail.com>
2021-03-10 14:22:42 -0600

    Omiited portfolio grid css and converted over to .js

--- src/components/PortfolioGrid.js
+++ src/components/PortfolioGrid.js
@@ -1,28 +1,105 @@
 import React, { Component } from 'react';
-import '../css/portfolio-grid.css';
 import { motion } from 'framer-motion';
-import { transition, cardUp, cardUpContainer } from '../animations';
-import styled from 'styled-components';
+import { transition, cardUp } from '../animations';
+import styled, { keyframes } from 'styled-components';
 import { Link } from 'react-router-dom';
 
 
 const GridNav = styled(motion.div)`
+  display: flex;
+  justify-content: center;
+  margin: 30px 0 40px 0;
+
+  ul {
     display: flex;
+    flex-wrap: wrap;
     justify-content: center;
-    margin: 30px 0 40px 0;
+    align-items: center;
+    margin: 0 auto;
+    list-style: none;
+    padding: 0 15vw;
+  }
+
+  li {
+    margin: 10px 10px;
+  }
 `;
 
 
-const PortLink = styled(Link)`
+
+const Label = styled.label`
+  color: #fff;
+  font-size: clamp(1.5rem, 1.7vw, 2.5rem);
+  font-weight: 700;
+  padding: 5px 10px;
+  transition: .3s;
+  cursor: pointer;
+`;
+
+const GridNavInput = styled.input`
+  display: none;
+
+  &:checked + ${Label} {
+    color: #444;
+    padding: 5px 10px;
+  }
+
+`;
+
+const PortLink = styled(Link)``;
+
+const CardsContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+  list-style: none;
+  padding: 0 15vw;
+
+  div {
+    margin: 10px 10px;
+  }
+`;
+
+const show = keyframes`
+  0% {opacity: 0;}
+  100% {opacity: 1;}
+`;
+
+const Figure = styled.figure`
+  overflow: hidden;
+  margin: 0 5px;
+  animation: ${show} .8s ease;
+
+  img {
+    width: clamp(350px, 22vw, 400px);
+    height: clamp(400px, 500px, 500px);
+    left: 0;
+    object-fit: cover;
+    box-shadow: 0 10px 15px #a8b4fc30;
+  }
+`;
+
+const FigCaption = styled.figcaption`
+  position: absolute;
+  width: 100%;
+  color: #ddd;
+  left: 0;
+  bottom: 0;
+
+  
 
 `;
 
 const imgs = [{description:"Wordpress, Elementor, WooCommerce", tag:"WEB DEVELOPMENT", page:"/mendedmoments", src:"http://darrinwells.com/img/thumbnails/mendedmoments_thumb.jpg"},
-              {description:"Wordpress, Elementor", tag:"WEB DEVELOPMENT", page:"/medical-device-success", src:""},
+              {description:"Wordpress, Elementor", tag:"WEB DEVELOPMENT", page:"/medical-device-success", src:"https://www.medicaldevicesuccess.com/wp-content/uploads/2019/07/mds-logo1.png"},
               {description:"Network Solutions CMS, SEO, Inventory Management", tag:"WEB DEVELOPMENT", page:"/discount-office-source", src:"http://darrinwells.com/img/thumbnails/dos_thumb.jpg"},
               {description:"Wordpress, WPBakery Page Builder", tag:"WEB DEVELOPMENT", page:"/TNT-Automart", src:"http://darrinwells.com/img/thumbnails/tnt_thumb.jpg"},
+              {description:"Wordpress, Elementor", tag:"WEB DEVELOPMENT", page:"/SRD-Vision", src:""},
               {description:"Adobe Illustrator", tag:"DESIGN", page:"/briskethouse", src:"http://darrinwells.com/img/thumbnails/brisket_thumb.jpg"},
               {description:"Adobe Illustrator", tag:"DESIGN", page:"/mendedmoments", src:"http://darrinwells.com/img/thumbnails/plastic_thumb.jpg"},
+              {description:"Adobe Illustrator", tag:"DESIGN", page:"/wild-acreage", src:""},
               {description:"Adobe Photoshop, FlexiSign Pro", tag:"DESIGN", page:"/ace-automotive", src:"http://darrinwells.com/img/thumbnails/aceauto_thumb.jpg"},
               {description:"Adobe Illustrator", tag:"DESIGN", page:"/fontana", src:"http://darrinwells.com/img/thumbnails/fontana_thumb.jpg"},
               {description:"Adobe Photoshop", tag:"DESIGN", page:"/photo-restoration", src:"http://darrinwells.com/img/thumbnails/fontana_thumb.jpg"},
@@ -49,39 +126,41 @@ const Filters = ({onClickAll, all, onClick, filters}) =>
       transition={transition}>
     <ul>
       <li onClick={onClickAll}>
-        <input 
+        <GridNavInput 
           type="checkbox"
           checked={all}
         />
-        <label htmlFor="all">ALL</label>
+        <Label htmlFor="all">ALL</Label>
       </li>
        {filters.map(
         (filter, i)=>
           <li  key={i} data-index={i} onClick={onClick} >
-            <input 
+            <GridNavInput 
               id={filter.name} 
               type="checkbox" 
               checked={filter.status}
             />
-            <label htmlFor={filter.name}>{filter.name}</label>
+            <Label htmlFor={filter.name}>{filter.name}</Label>
           </li>)}
     </ul>
   </GridNav>
 
 const Cards = ({imgs}) =>
-  <ul>
+  <CardsContainer>
     {imgs.map(
       (img, i)=>
-      <li  key={i}>
-        <PortLink to={img.page}><figure>
-          <img src={img.src} alt={img.description}/>
-          <figcaption> 
-            <div>{img.description} </div>
-            <span>{img.tag}</span>
-          </figcaption>
-        </figure></PortLink>
-      </li>)}
-  </ul>
+      <div key={i}>
+        <PortLink to={img.page}>
+          <Figure>
+            <img src={img.src} alt={img.description}/>
+            <FigCaption> 
+              <div>{img.description} </div>
+              <span>{img.tag}</span>
+            </FigCaption>
+          </Figure>
+        </PortLink>
+      </div>)}
+  </CardsContainer>
 
 class PortfolioGrid extends React.Component{
   state ={
